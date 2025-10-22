cb958f748bc35738e5651ca20e70af96578dbcd8
siham <siham.emf35@gmail.com>
2025-10-22 03:01:07 +0100

    database connexion files

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'petitions');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('SITE_NAME', 'Gestion des Pétitions');
+define('SESSION_TIMEOUT', 1800);  //pour auth.php
+?>
---
+++ test_connexion.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'includes/database.php';
+echo "Connexion réussie !";
+?>

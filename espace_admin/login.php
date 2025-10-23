b7dfe4e3181cb98fd99c8b80bd7b77bb898a3820
siham <siham.emf35@gmail.com>
2025-10-23 03:13:11 +0100

    changements dans espace admin

--- espace_admin/login.php
+++ espace_admin/login.php
@@ -379,9 +379,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <header>
                 <div class="header-content">
                     <div class="logo">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                        </svg>
+                        <svg class="logo-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/>
+    <polyline points="14 2 14 8 20 8"/>
+    <line x1="16" y1="13" x2="8" y2="13"/>
+    <line x1="16" y1="17" x2="8" y2="17"/>
+    <line x1="10" y1="9" x2="8" y2="9"/>
+</svg>
                     </div>
                     <div class="header-text">
                         <h1>CitoyenVoix</h1>
